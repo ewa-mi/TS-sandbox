a41b7fcc841a0422eb995daf70f81c602bd6148a
ewami <efili1989@gmail.com>
2020-08-02 21:04:11 +0200

    make json and model with definded type of data for itemPost

---
+++ src/model.ts
@@ -0,0 +1,10 @@
+export type ItemPost = {
+  id: number;
+  name: string;
+  description: string;
+  alternatives: string[];
+  category: string;
+  writtenIn: string;
+  icon: string;
+  type: string;
+};
