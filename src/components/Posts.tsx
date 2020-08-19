59aa453fa339c2a29a9aa486aa6ba41ea4db4a88
ewami <efili1989@gmail.com>
2020-08-19 12:53:00 +0200

    add filters to the posts

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,34 +1,41 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { ItemPost } from "./model";
+import Filters from "./Filters";
 import data from "../posts.json";
 import "../scss/posts.scss";
 
-const Posts: FC = () => (
-  <div>
-    <h1>POSTS</h1>
-    <div className="container">
-      {data.map((item: ItemPost) => (
-        <div className="post" key={item.id}>
-          <h2>{item.tool}</h2>
-          <i className={item.icon}></i>
-          <div className="tags-area">
-            <p className="tag">{item.type}</p>
-            <p className="tag">{item.category}</p>
+const Posts: FC = () => {
+  const [filters, setFilters] = useState<string[]>([]);
+  return (
+    <div>
+      <h1>POSTS</h1>
+
+      <Filters filters={filters} setFilters={setFilters} />
+
+      <div className="container">
+        {data.map((item: ItemPost) => (
+          <div className="post" key={item.id}>
+            <h2>{item.tool}</h2>
+            <i className={item.icon}></i>
+            <div className="tags-area">
+              <p className="tag">{item.type}</p>
+              <p className="tag">{item.category}</p>
+            </div>
+            <div className="post-text">
+              <p>{item.description}</p>
+              <p className="hr">Written in: {item.writtenIn}</p>
+              <h3>Alternatives:</h3>
+              {item.alternatives.map((alternative, index) => (
+                <p key={index} className="alternatives">
+                  {alternative}
+                </p>
+              ))}
+            </div>
           </div>
-          <div className="post-text">
-            <p>{item.description}</p>
-            <p className="hr">Written in: {item.writtenIn}</p>
-            <h3>Alternatives:</h3>
-            {item.alternatives.map((alternative, index) => (
-              <p key={index} className="alternatives">
-                {alternative}
-              </p>
-            ))}
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Posts;
